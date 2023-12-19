8b0d5c5596280d26d99d940a3b2b68a985bd766b
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-19 17:13:29 -0500

    f

--- proyecto-arquitectura-frontEnd/src/app/movimiento-inventario/guardar-movimiento/guardar-movimiento.component.ts
+++ proyecto-arquitectura-frontEnd/src/app/movimiento-inventario/guardar-movimiento/guardar-movimiento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Movimiento } from '../movimiento';
 import { MovimientoService } from '../movimiento.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-guardar-movimiento',
@@ -24,13 +25,49 @@ export class GuardarMovimientoComponent implements OnInit{
   }
 
   public guardarMovimientos(){
-    this.service.guardarMovimientos(this.producto_id, this.movimientos).subscribe({
-      next:(datos)=>{this.irListaMovimientos()}
-    })
+    if(this.validarFormulario()){
+      this.service.guardarMovimientos(this.producto_id, this.movimientos).subscribe({
+        next:(datos)=>{this.irListaMovimientos()}
+      })
+
+    }
+    
   };
 
   irListaMovimientos(){
     this.router.navigate(['/movimientos-productos-categorias'])
+  }
+
+  validarFormulario() {
+    // Validación de solo texto en el campo nombre
+  
+    // Validación de campos vacíos
+    if (!this.movimientos.cantidad || !this.movimientos.fecha_movimiento || !this.movimientos.tipo_movimiento) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Por favor, completa todos los campos correctamente.',
+      });
+      return false;
+    }
+  
+    // Resto de la lógica si el formulario es válido
+    // ...
+  
+    // Alerta de éxito
+    Swal.fire({
+      icon: 'success',
+      title: 'Éxito',
+      text: 'Formulario validado correctamente. ¡Puedes continuar!',
+      confirmButtonText: 'Aceptar' // Cambia el texto del botón Aceptar según tus necesidades
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Redirige a otra página después de hacer clic en Aceptar
+        this.router.navigate(['/movimientos-productos-categorias']); // Reemplaza '/otra-pagina' con la ruta de la página a la que deseas redirigir
+      }
+    });
+  
+    return true;
   }
 
 }
