db17ffc9ff678569837bfb2f53a909b07c99d96f
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-19 15:50:36 -0500

    Update guardar-producto.component.ts

--- proyecto-arquitectura-frontEnd/src/app/productos/guardar-producto/guardar-producto.component.ts
+++ proyecto-arquitectura-frontEnd/src/app/productos/guardar-producto/guardar-producto.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Productos } from '../productos';
 import { ProductosServicesService } from '../productos-services.service';
 import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-guardar-producto',
@@ -12,7 +13,7 @@ export class GuardarProductoComponent {
   //
   productos:Productos=new Productos();
 
-  constructor(private service:ProductosServicesService){}
+  constructor(private service:ProductosServicesService, private router:Router){}
 
   //guardarProducto
   public guardarProducto(){
@@ -20,6 +21,7 @@ export class GuardarProductoComponent {
       this.service.guardarProducto(this.productos).subscribe({
         next:(datos)=>{
           //ir a la pagina
+
           console.log(datos)
         }
       })
@@ -28,19 +30,35 @@ export class GuardarProductoComponent {
 
   //
   validarFormulario() {
+    // Validación de solo texto en el campo nombre
+    const soloTextoRegex = /^[a-zA-Z\s]+$/;
+    if (!soloTextoRegex.test(this.productos.nombre)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'El campo de nombre solo puede contener letras y espacios.',
+      });
+      return false;
+    }
+  
+    // Validación de campos vacíos
     if (!this.productos.nombre || !this.productos.precio || !this.productos.stock) {
       Swal.fire({
         icon: 'error',
         title: 'Error',
         text: 'Por favor, completa todos los campos correctamente.',
       });
-      return false; 
+      return false;
     }
   
     // Resto de la lógica si el formulario es válido
     // ...
   
-    return true; 
+    return true;
+  }
+
+  irPagina(){
+
   }
 
 
