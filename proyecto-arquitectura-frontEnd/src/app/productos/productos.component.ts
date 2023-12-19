be94065b482d451b2cc48994e3be40ef45719adf
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-19 16:02:53 -0500

    Update productos.component.ts

--- proyecto-arquitectura-frontEnd/src/app/productos/productos.component.ts
+++ proyecto-arquitectura-frontEnd/src/app/productos/productos.component.ts
@@ -3,6 +3,7 @@ import { Productos } from './productos';
 import { ProductosServicesService } from './productos-services.service';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-productos',
@@ -43,13 +44,42 @@ export class ProductosComponent implements OnInit,OnDestroy {
     this.enrutador.navigate(['editar-producto',id])
   }
 
-  eliminarProducto(id:number){
-    this.service.elmimarProductos(id).subscribe(
-      {
-      next:(datos) =>this.listarProductos(),
-      error:(errores:any) => console.log(errores)
-    }
-    );
+  // eliminarProducto(id:number){
+  //   this.service.elmimarProductos(id).subscribe(
+  //     {
+  //     next:(datos) =>this.listarProductos(),
+  //     error:(errores:any) => console.log(errores)
+  //   }
+  //   );
+  // }
+  eliminarProducto(id: number) {
+    // Mostrar la alerta de confirmación
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: '¡No podrás revertir esto!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminarlo'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Si el usuario hace clic en "Sí, eliminarlo", proceder con la eliminación
+        this.service.elmimarProductos(id).subscribe(
+          () => {
+            // Mostrar una alerta de éxito
+            Swal.fire('¡Eliminado!', 'La estación ha sido eliminada.', 'success');
+            // Volver a cargar las estaciones después de la eliminación
+            this.listarProductos();
+          },
+          (error) => {
+            console.error('Error al eliminar estación:', error);
+            // Mostrar una alerta de error en caso de fallo en la eliminación
+            Swal.fire('Error', 'Hubo un error al eliminar la estación.', 'error');
+          }
+        );
+      }
+    });
   }
 
 }
